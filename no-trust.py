873f8b5916498ac67e5cf03430decac3ed721474
Eric <eyangch@gmail.com>
2025-03-27 05:01:13 +0000

    remove proxy comments

--- no-trust.py
+++ no-trust.py
@@ -1,16 +1,3 @@
-#!/usr/bin/env python3
-
-# Simple TCP proxy server
-# 
-# This script can be used to route traffic from one host to another.
-#
-# Example:
-# proxy.py 127.0.0.1 8080 example.org 80
-#
-# Running the command above will start a TCP server listening on 127.0.0.1:8080.
-# When a client connects to it, it will start a connection to example.org:80, and
-# proxy the data from the client to the target, and from the target back to the client.
-
 import asyncio
 from websockets.asyncio.server import serve
 from websockets.exceptions import ConnectionClosedOK
